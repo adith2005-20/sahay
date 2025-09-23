372c2714c8e353e00f1dbbb70d4459c72077f118
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-09-23 12:00:03 +0530

    fix(ui): childless pages fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-background text-white">
+        <Link href={'/dashboard'}>
+        <Button>Go to dashboard</Button>
+        </Link>
+        
       </main>
     </HydrateClient>
   );
