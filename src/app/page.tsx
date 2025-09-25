be0a9649856e5159fda5d0b611b7bc04f9d23b80
aryansharma2k4 <aryansharma2k4@gmail.com>
2025-09-25 17:37:07 +0530

    made it ready for merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,4 +12,3 @@ export default async function Home() {
     </HydrateClient>
   );
 }
-                                               b        n                                                                                              
